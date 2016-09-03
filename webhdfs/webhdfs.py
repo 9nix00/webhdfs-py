3376ccf5d07ee0aabc08484be39258466c731217
WANG WENPEI <wangwenpei@nextoa.com>
2016-09-03 13:58:01 +0800

    fix java dns error

--- webhdfs/webhdfs.py
+++ webhdfs/webhdfs.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(name='webhdfs')
 
 WEBHDFS_CONTEXT_ROOT="/webhdfs/v1"
 
-class WebHDFS(object):       
+class WebHDFS(object):
     """ Class for accessing HDFS via WebHDFS 
     
         To enable WebHDFS in your Hadoop Installation add the following configuration
@@ -24,17 +24,17 @@ class WebHDFS(object):
     
         see: https://issues.apache.org/jira/secure/attachment/12500090/WebHdfsAPI20111020.pdf
     """
-    
+
     def __init__(self, namenode_host, namenode_port, hdfs_username):
         self.namenode_host=namenode_host
         self.namenode_port = namenode_port
         self.username = hdfs_username
-        
-    
+
+
     def mkdir(self, path):
         if os.path.isabs(path)==False:
             raise Exception("Only absolute paths supported: %s"%(path))
-        
+
         url_path = WEBHDFS_CONTEXT_ROOT + path +'?op=MKDIRS&user.name='+self.username
         logger.debug("Create directory: " + url_path)
         httpClient = self.__getNameNodeHTTPClient()
@@ -42,12 +42,12 @@ class WebHDFS(object):
         response = httpClient.getresponse()
         logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
         httpClient.close()
-        
-        
+
+
     def rmdir(self, path):
         if os.path.isabs(path)==False:
             raise Exception("Only absolute paths supported: %s"%(path))
-        
+
         url_path = WEBHDFS_CONTEXT_ROOT + path +'?op=DELETE&recursive=true&user.name='+self.username
         logger.debug("Delete directory: " + url_path)
         httpClient = self.__getNameNodeHTTPClient()
@@ -55,29 +55,34 @@ class WebHDFS(object):
         response = httpClient.getresponse()
         logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
         httpClient.close()
-     
-     
+
+
     def put(self, source_data, target_path, replication=1):
         if os.path.isabs(target_path)==False:
             raise Exception("Only absolute paths supported: %s"%(target_path))
-        
+
         url_path = WEBHDFS_CONTEXT_ROOT +  target_path + '?op=CREATE&overwrite=true&user.name='+self.username
-        
+
         httpClient = self.__getNameNodeHTTPClient()
         httpClient.request('PUT', url_path , headers={})
         response = httpClient.getresponse()
         logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
         msg = response.msg
+
+        if msg["location"].find("http://localhost") == 0:
+            msg["location"].replace("http://localhost", "http://" + self.namenode_host)
+            pass
+
         redirect_location = msg["location"]
         logger.debug("HTTP Location: %s"%(redirect_location))
         result = urlparse.urlparse(redirect_location)
         redirect_host = result.netloc[:result.netloc.index(":")]
         redirect_port = result.netloc[(result.netloc.index(":")+1):]
         # Bug in WebHDFS 0.20.205 => requires param otherwise a NullPointerException is thrown
-        redirect_path = result.path + "?" + result.query + "&replication="+str(replication) 
-            
+        redirect_path = result.path + "?" + result.query + "&replication="+str(replication)
+
         logger.debug("Send redirect to: host: %s, port: %s, path: %s "%(redirect_host, redirect_port, redirect_path))
-        fileUploadClient = httplib.HTTPConnection(redirect_host, 
+        fileUploadClient = httplib.HTTPConnection(redirect_host,
                                                   redirect_port, timeout=600)
         # This requires currently Python 2.6 or higher
         fileUploadClient.request('PUT', redirect_path, source_data, headers={})
@@ -85,9 +90,9 @@ class WebHDFS(object):
         logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
         httpClient.close()
         fileUploadClient.close()
-        return response.status 
-    
-    
+        return response.status
+
+
     def get(self, source_path):
         if os.path.isabs(source_path)==False:
             raise Exception("Only absolute paths supported: %s"%(source_path))
@@ -105,28 +110,28 @@ class WebHDFS(object):
             result = urlparse.urlparse(redirect_location)
             redirect_host = result.netloc[:result.netloc.index(":")]
             redirect_port = result.netloc[(result.netloc.index(":")+1):]
-            
-            redirect_path = result.path + "?" + result.query  
-                
+
+            redirect_path = result.path + "?" + result.query
+
             logger.debug("Send redirect to: host: %s, port: %s, path: %s "%(redirect_host, redirect_port, redirect_path))
-            fileDownloadClient = httplib.HTTPConnection(redirect_host, 
+            fileDownloadClient = httplib.HTTPConnection(redirect_host,
                                                       redirect_port, timeout=600)
-            
+
             fileDownloadClient.request('GET', redirect_path, headers={})
             response = fileDownloadClient.getresponse()
             logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
-            data=response.read()            
-        httpClient.close()        
+            data=response.read()
+        httpClient.close()
         return data
-    
-         
+
+
     def copyFromLocal(self, source_path, target_path, replication=1):
         f = open(source_path, "r")
         source_data = f.read()
         f.close()
         return self.put(source_data, target_path, replication)
-               
-        
+
+
     def copyToLocal(self, source_path, target_path):
         if os.path.isabs(source_path)==False:
             raise Exception("Only absolute paths supported: %s"%(source_path))
@@ -145,17 +150,17 @@ class WebHDFS(object):
             result = urlparse.urlparse(redirect_location)
             redirect_host = result.netloc[:result.netloc.index(":")]
             redirect_port = result.netloc[(result.netloc.index(":")+1):]
-            
-            redirect_path = result.path + "?" + result.query  
-                
+
+            redirect_path = result.path + "?" + result.query
+
             logger.debug("Send redirect to: host: %s, port: %s, path: %s "%(redirect_host, redirect_port, redirect_path))
-            fileDownloadClient = httplib.HTTPConnection(redirect_host, 
+            fileDownloadClient = httplib.HTTPConnection(redirect_host,
                                                       redirect_port, timeout=600)
-            
+
             fileDownloadClient.request('GET', redirect_path, headers={})
             response = fileDownloadClient.getresponse()
             logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
-            
+
             # Write data to file
             target_file = open(target_path, "w")
             target_file.write(response.read())
@@ -164,15 +169,15 @@ class WebHDFS(object):
         else:
             target_file = open(target_path, "w")
             target_file.close()
-            
-        httpClient.close()        
+
+        httpClient.close()
         return response.status
-     
-     
+
+
     def listdir(self, path):
         if os.path.isabs(path)==False:
             raise Exception("Only absolute paths supported: %s"%(path))
-        
+
         url_path = urllib.quote(WEBHDFS_CONTEXT_ROOT + path+'?op=LISTSTATUS&user.name='+self.username)
         logger.debug("List directory: " + url_path)
         httpClient = self.__getNameNodeHTTPClient()
@@ -181,31 +186,31 @@ class WebHDFS(object):
         logger.debug("HTTP Response: %d, %s"%(response.status, response.reason))
         data_dict = json.loads(response.read())
         logger.debug("Data: " + str(data_dict))
-        files=[]        
+        files=[]
         for i in data_dict["FileStatuses"]["FileStatus"]:
-            logger.debug(i["type"] + ": " + i["pathSuffix"]) 
-            files.append(i["pathSuffix"])        
+            logger.debug(i["type"] + ": " + i["pathSuffix"])
+            files.append(i["pathSuffix"])
         httpClient.close()
         return files
-    
+
     def __getNameNodeHTTPClient(self):
-        httpClient = httplib.HTTPConnection(self.namenode_host, 
-                                            self.namenode_port, 
+        httpClient = httplib.HTTPConnection(self.namenode_host,
+                                            self.namenode_port,
                                                        timeout=600)
         return httpClient
-    
-    
-    
-if __name__ == "__main__":      
+
+
+
+if __name__ == "__main__":
     webhdfs = WebHDFS("localhost", 50070, "luckow")
     webhdfs.mkdir("/pilotstore-1/pd-9c2d42c4-30a3-11e1-bab1-00264a13ca4c/")
-    webhdfs.copyFromLocal("/Users/luckow/workspace-saga/applications/pilot-store/test/data1/test1.txt", 
+    webhdfs.copyFromLocal("/Users/luckow/workspace-saga/applications/pilot-store/test/data1/test1.txt",
                               "/pilotstore-1/pd-9c2d42c4-30a3-11e1-bab1-00264a13ca4c/test1.txt")
-    
+
     webhdfs.copyToLocal("/pilotstore-1/pd-9c2d42c4-30a3-11e1-bab1-00264a13ca4c/test1.txt",
                         "/tmp/test1.txt")
-    
+
     webhdfs.listdir("/")
-        
-        
-        
+
+
+
